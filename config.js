05622a5b77ffef3620f786e42836a8e8b94fbf7b
rodgeferrer29-netizen <rodgeferrer29@gmail.com>
2026-02-06 20:01:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉😳😻😸",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! I'm the luckiest boy in the world! 🎉😳😻😸",
+        message: "I LOVE YOU SO MUCH MY LOVE THANKYOU FOR BEING MY VALENTINE",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
